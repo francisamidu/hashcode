a362213281939d81d4b51f0cc9864137a07e1b95
Francis Amidu <francisamidu124@gmail.com>
2025-03-19 21:28:33 +0200

    chore:minor section heading fix

--- src/components/SectionHeading.tsx
+++ src/components/SectionHeading.tsx
@@ -6,7 +6,7 @@ type SectionHeadingProps = {
 }
 const SectionHeading = ({ textNormal, textStripped }: SectionHeadingProps) => {
   return (
-    <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 text-center sm:text-start sm:text-4xl">
+    <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 text-center sm:text-4xl">
       {textNormal}{' '}
       <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-700 to-indigo-600">
         {textStripped}
