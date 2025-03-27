cfe5aeeeb2cb8464b437c60b0880e5ee52cebdc7
Francis Amidu <francisamidu124@gmail.com>
2025-03-27 20:19:28 +0200

    chore:archived the auth bug for later fix

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/ReactToastify.css'
+import '@/styles/main.css'
 
 // Create a client
 const queryClient = new QueryClient()
--- src/routes/auth/index.tsx
+++ src/routes/auth/index.tsx
@@ -2,18 +2,15 @@ import AltFooter from '@/components/AltFooter'
 import AuthNavbar from '@/components/AuthNavbar'
 import ErrorFallback from '@/components/ErrorFallback'
 import { createRootRoute, Outlet } from '@tanstack/react-router'
-import { ErrorBoundary } from 'react-error-boundary'
 
 const Index = () => {
   return (
     <>
-      <ErrorBoundary FallbackComponent={ErrorFallback}>
-        <AuthNavbar />
-        <main>
-          <Outlet />
-        </main>
-        <AltFooter />
-      </ErrorBoundary>
+      <AuthNavbar />
+      <main>
+        <Outlet />
+      </main>
+      <AltFooter />
     </>
   )
 }
